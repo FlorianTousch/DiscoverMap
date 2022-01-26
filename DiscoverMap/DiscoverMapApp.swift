f00c8a0e1f0ebac9a444fa5dacfb509c6cf00f37
FlorianTo <florian.tousch@hotmail.fr>
2022-01-26 16:36:58 +0100

    Initial Commit

---
+++ DiscoverMap/DiscoverMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoverMapApp.swift
+//  DiscoverMap
+//
+//  Created by Florian on 26/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct DiscoverMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
